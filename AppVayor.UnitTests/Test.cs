020808d09487a4a3bcf3e2a29c75801e0a44b2f9
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2015-10-07 14:23:35 -0300

    no message

---
+++ AppVayor.UnitTests/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AppVayor.UnitTests
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void TestCase ()
+		{
+			Assert.IsTrue (true);
+		}
+	}
+}
+
--- AppVayorTest/MyClass.cs
+++ AppVayorTest/MyClass.cs
@@ -3,14 +3,8 @@ using NUnit.Framework;
 
 namespace AppVayorTest
 {
-	[TestFixture]
 	public class MyClass
 	{
-		[Test]
-		public void MeuTesteVerdadeiro ()
-		{
-			Assert.IsTrue(true);
-		}
 	}
 }
 
