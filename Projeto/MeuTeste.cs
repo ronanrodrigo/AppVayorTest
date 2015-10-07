423d79b1ef1a72f66172755b81466f660f1261f4
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2015-10-07 13:31:57 -0300

    a

---
+++ Projeto/MeuTeste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Projeto
+{
+	[TestFixture]
+	public class MeuTeste
+	{
+		[Test]
+		public void TestaSeVerdadeiro()
+		{
+			Assert.IsTrue(true);
+		}
+	}
+}
